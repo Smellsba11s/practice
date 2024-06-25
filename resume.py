a797398d092c8aab73b48d4c2b06f577e0ebae0b
Лень Матвей <146654131+Smellsba11s@users.noreply.github.com>
2024-06-25 12:20:12 +0300

    добавил фильтр в ссылке

--- resume.py
+++ resume.py
@@ -3,10 +3,50 @@ from bs4 import BeautifulSoup
 import fake_useragent
 import json
 import time
+from urllib.parse import urlencode, urlunparse, urlparse
 
-def get_links(keyword):
+def get_links(keyword, experience_filters=None, schedule_filters=None, education_filters=None, salary_from=None, salary_to=None):
+    base_url = 'https://hh.ru/search/resume'
+    params = {
+        'text': keyword,
+        'area': '1',
+        'isDefaultArea': 'true',
+        'exp_period': 'all_time',
+        'logic': 'normal',
+        'pos': 'full_text',
+        'page': '0',
+        'search_period': '0',
+        'order_by': 'relevance',
+        'filter_exp_period': 'all_time',
+        'relocation': 'living_or_relocation',
+        'gender': 'unknown',
+        'job_search_status_changed_by_user': 'true'
+    }
+
+   # Добавление параметров зарплаты, если они указаны
+    if salary_from is not None:
+        params['salary_from'] = salary_from
+    if salary_to is not None:
+        params['salary_to'] = salary_to
+    
+    # Если оба значения зарплаты указаны, добавить параметр only_with_salary
+    if salary_from is not None and salary_to is not None:
+        params['label'] = 'only_with_salary'
+    # Создание URL без experience, schedule и education фильтров
+    url_parts = list(urlparse(base_url))
+    query = urlencode(params)
+    url_parts[4] = query
+
+    # Добавление experience, schedule и education фильтров, если они указаны
+    url = urlunparse(url_parts)
+    if experience_filters:
+        url += ''.join([f'&experience={exp}' for exp in experience_filters])
+    if schedule_filters:
+        url += ''.join([f'&schedule={sched}' for sched in schedule_filters])
+    if education_filters:
+        url += ''.join([f'&education_level={edu}' for edu in education_filters])
+    
     user_agent = fake_useragent.UserAgent()
-    url = f'https://hh.ru/search/resume?text={keyword}&area=1&isDefaultArea=true&exp_period=all_time&logic=normal&pos=full_text&page=0'
     
     try:
         response = requests.get(url, headers={'user-agent': user_agent.random})
@@ -19,10 +59,10 @@ def get_links(keyword):
     
     for page in range(page_count):
         try:
-            url = f'https://hh.ru/search/resume?text={keyword}&area=1&isDefaultArea=true&exp_period=all_time&logic=normal&pos=full_text&page={page}'
+            url = url + f'&page={page}'
             response = requests.get(url, headers={'user-agent': user_agent.random})
             response.raise_for_status()  
-            
+            print(url)
             soup = BeautifulSoup(response.content, 'lxml')
             links = [f"https://hh.ru{a['href'].split('?')[0]}"
                      for a in soup.find_all("a", attrs={"rel": "nofollow"}) 
@@ -65,10 +105,17 @@ def get_resume(link):
 
 if __name__ == "__main__": 
     data = []
-    for link in get_links('python'): # чисто условность, пользователь пишет что-то вместо python
+    keyword = "python"
+    experience_filters = ["between1And3", "noExperience", "moreThan6", "between3And6"]
+    schedule_filters = ["fullDay", "remote", "flexible", "shift", "flyInFlyOut"]
+    education_filters = ["higher", "unfinished_higher", "master", "bachelor", "special_secondary"]
+    salary_from = 100000
+    salary_to = 200000
+    links = get_links(keyword, experience_filters, schedule_filters, education_filters, salary_from, salary_to)
+    for link in links: # чисто условность, пользователь пишет что-то вместо python
         resume = get_resume(link)
         if resume:
             data.append(resume)
             time.sleep(0.5) 
-            with open('data.json', 'w', encoding='utf-8') as file:
+            with open('resume.json', 'w', encoding='utf-8') as file:
                 json.dump(data, file, indent=4, ensure_ascii=False)
